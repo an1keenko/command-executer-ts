1dfd327221212d45acdcfc1ae0ebaff7a7385dca
Ihar Anikeyenka <ianikeyenka@productiveedge.com>
2023-11-06 17:30:02 +0100

    Input processed, regress of the inquirer's version and installed prettier and eslint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
+import { PromptService } from './core/prompt/prompt.service'
+
 export class App {
-    run() {
-        console.log('Done')
-    }
+  async run() {
+    const res = await (new PromptService().input<number>('Number', 'number'))
+    console.log(res)
+  }
 }
 
 const app = new App()
